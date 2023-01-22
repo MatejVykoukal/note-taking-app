6b6da86747241606bf99613f778f2861b13961a9
MatejVykoukal <matej.vykoukal@budgetbakers.com>
2023-01-22 17:38:52 +0100

    Init commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { MantineProvider } from "@mantine/core";
+
+import { api } from "../utils/api";
+
+import "../styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <MantineProvider withGlobalStyles>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </MantineProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
