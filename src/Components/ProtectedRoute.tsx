722fb60d133593552e1d3dbb5ff559751cba8175
rollakal <39028830+rollakal@users.noreply.github.com>
2023-01-31 19:05:54 +0100

    feat: Add loading component

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,14 +1,16 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { type FC, useEffect, type ReactNode } from "react";
+import { type FC, type ReactNode, useEffect, useState } from "react";
+import { Loader } from "@mantine/core";
 
 interface Props {
   children: ReactNode;
 }
 
 const ProtectedRoute: FC<Props> = ({ children }) => {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!session) {
@@ -16,7 +18,25 @@ const ProtectedRoute: FC<Props> = ({ children }) => {
     }
   }, []);
 
-  return <>{children}</>;
+  useEffect(() => {
+    if (status === "loading") {
+      setIsLoading(true);
+    }
+  }, [status]);
+
+  const renderProtectedPage = () => {
+    if (isLoading) {
+      return (
+        <div className="flex h-screen w-full items-center justify-center">
+          <Loader color="gray" />
+        </div>
+      );
+    } else {
+      return children;
+    }
+  };
+
+  return <>{renderProtectedPage()}</>;
 };
 
 export default ProtectedRoute;
