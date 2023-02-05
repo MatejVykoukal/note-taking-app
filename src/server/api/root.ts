09f2958b85213cf834990098077e861c2d0a8c9a
rollakal <39028830+rollakal@users.noreply.github.com>
2023-02-05 10:53:18 +0100

    feat: Added api create, read, delete routes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
+import { notesRouter } from "./routers/notes";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { exampleRouter } from "./routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  notes: notesRouter
 });
 
 // export type definition of API
--- src/server/api/routers/notes.ts
+++ src/server/api/routers/notes.ts
@@ -1,10 +1,51 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const notesRouter = createTRPCRouter({
-  getNotes: publicProcedure.query(async ({ ctx }) => {
-    const comps = await ctx.prisma.note.findMany()
-    return comps
-  }),
+  createNote: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        note: z.string(),
+        userId: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const note = await ctx.prisma.note.create({
+        data: {
+          title: input.title,
+          note: input.note,
+          userId: input.userId,
+        },
+      });
+
+      return note;
+    }),
+
+  getNotes: protectedProcedure.input(z.object({ userId: z.string() })).query(
+    async ({ ctx, input }) => {
+      const notes = await ctx.prisma.note.findMany({
+        where: {
+          userId: input.userId,
+        },
+      });
+      return notes;
+    }
+  ),
+  deleteNote: protectedProcedure
+    .input(
+      z.object({
+        id: z.string()
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const note = await ctx.prisma.note.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return note;
+    }),
 });
